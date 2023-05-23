97fded44619365837c7836e9e1f3bc2b1e5cfbb9
Rakesh Shah <113095431+rakatheintermediatecoder@users.noreply.github.com>
2023-05-23 22:14:30 +0530

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -30,7 +30,7 @@ const Contact = () => {
             <MdOutlineEmail className='contact__option-icon' />
             <h4>Email</h4>
             <h5>21amtics464@gmail.com</h5>
-            <a href="mailto:21amtics464@gmail.com" target='_blank'>Send a message</a>
+            <a href="mailto:dumiemail101@gmail.com" target='_blank'>Send a message</a>
           </article>
 
           <article className='contact__option'>

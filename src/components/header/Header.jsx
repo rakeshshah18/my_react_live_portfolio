7daeddf203ae57e29a53ad9acbc44fcf795b292c
rakeshshah18 <21amtics464@gmail.com>
2024-06-18 20:43:37 +0530

    miner changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hi I'm</h5>
         <h1>Rakesh Shah</h1>
-        <h5 className="text-light">A Good Developer</h5>
+        <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
         <HeaderSocials />
         <div className="me">

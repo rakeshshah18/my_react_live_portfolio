94b21b28f44b4a9cbf3c535d25be591128d2570e
rakeshshah18 <21amtics464@gmail.com>
2025-01-29 11:46:19 +0530

    add recent work of cm and excel data view.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,33 +2,41 @@ import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/portfolio1.png'
 import IMG2 from '../../assets/portfolio2.png'
-import IMG3 from '../../assets/portfolio3.jpg'
+import IMG3 from '../../assets/portfolio3.png'
+import IMG4 from '../../assets/portfolio4.png'
 
 
 const data = [
 
   {
     id: 1,
-    image:IMG1,
-    title:'Portfolio',
+    image: IMG1,
+    title: 'Portfolio',
     github: 'https://github.com/rakeshshah18/my_react_live_portfolio',
-    demo:'https://rakesh-shah.vercel.app/'
+    demo: 'https://rakesh-shah.vercel.app/'
   },
-  
+
   {
     id: 2,
-    image:IMG2,
-    title:'Yogya Capital',
+    image: IMG2,
+    title: 'Yogya Capital',
     github: 'https://github.com/rakeshshah18/',
-    demo:'https://yogyacapital.com/'
+    demo: 'https://yogyacapital.com/'
   },
-  
+
   {
     id: 3,
-    image:IMG3,
-    title:'Lorem Ipsum',
-    github: '#',
-    demo:'#'
+    image: IMG3,
+    title: 'CM Placement Services',
+    github: 'https://github.com/LMS-Valsad/CM_PS',
+    demo: 'https://cm-placement-services.vercel.app/'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Excel Data View',
+    github: 'https://github.com/rakeshshah18/excel-view-react-app',
+    demo: 'https://excel-view-zeta.vercel.app/'
   }
 ]
 
@@ -41,7 +49,7 @@ const Portfolio = () => {
 
       <div className="container portfolio__container">
         {
-          data.map(({id,image,title,github,demo}) =>{
+          data.map(({ id, image, title, github, demo }) => {
             return (
               <article key={id} className='container portfolio__item'>
                 <div className="portfolio__item-image">
@@ -62,8 +70,8 @@ const Portfolio = () => {
             );
           })
         }
-      
-        
+
+
       </div>
 
 

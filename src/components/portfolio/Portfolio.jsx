61aa90d9c631ed9b48e8b9565344bc6054ded489
rakeshshah18 <21amtics464@gmail.com>
2025-07-03 10:31:16 +0530

    fixed error of af images

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,6 +4,7 @@ import IMG1 from '../../assets/portfolio1.png'
 import IMG2 from '../../assets/portfolio2.png'
 import IMG3 from '../../assets/portfolio3.jpg'
 import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/af.jpg'
 
 
 const data = [
